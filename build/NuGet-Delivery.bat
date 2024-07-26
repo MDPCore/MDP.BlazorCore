01ee04a5fd3445cb1a62dbcc2595a182affbf1ec
clark159 <clark159@hotmail.com>
2024-07-26 21:33:45 +0800

    [nuget] Release MDP.BlazorCore 8.0.7.32

--- build/NuGet-Delivery.bat
+++ build/NuGet-Delivery.bat
@@ -4,7 +4,7 @@ setlocal enabledelayedexpansion
 
 :: ================================================
 :: Variables
-set version=8.0.7.31
+set version=8.0.7.32
 
 set buildDir=%~dp0
 set srcDir=%~dp0..\src
