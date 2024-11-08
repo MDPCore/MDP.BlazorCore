8dbed2a15b9cbf60a98dd5cf1726f7e36ddf889c
clark159 <clark159@hotmail.com>
2024-11-08 20:35:27 +0800

    [src] 更新NuGet套件到最新版本

--- src/MDP.BlazorCore/Components/Routing/RoutesOptions.cs
+++ src/MDP.BlazorCore/Components/Routing/RoutesOptions.cs
@@ -36,7 +36,7 @@ namespace MDP.BlazorCore
         public Assembly[] CreateAdditionalAssemblies()
         {
             // AssemblyList
-            var assemblyList = this.FindAllApplicationAssembly();
+            var assemblyList = MDP.Reflection.Assembly.FindAllApplicationAssembly();
             if (assemblyList == null) return Array.Empty<Assembly>();
 
             // AppAssembly
@@ -45,83 +45,6 @@ namespace MDP.BlazorCore
 
             // Return
             return assemblyList.ToArray();
-        }
-
-        private List<Assembly> FindAllApplicationAssembly()
-        {
-            // AssemblyDictionary
-            var assemblyDictionary = new Dictionary<string, System.Reflection.Assembly>(StringComparer.OrdinalIgnoreCase);
-
-            //  LoadedAssemblyList
-            var loadedAssemblyList = AppDomain.CurrentDomain.GetAssemblies();
-            if (loadedAssemblyList == null) throw new InvalidOperationException($"{nameof(loadedAssemblyList)}=null");
-            foreach (var loadedAssembly in loadedAssemblyList)
-            {
-                if (assemblyDictionary.ContainsKey(loadedAssembly.Location) == false)
-                {
-                    assemblyDictionary[loadedAssembly.Location] = loadedAssembly;
-                }
-            }
-
-            // FileAssemblyPathList
-            var fileAssemblyPathList = MDP.IO.File.GetAllFilePath("*.dll");
-            if (fileAssemblyPathList == null) throw new InvalidOperationException($"{nameof(fileAssemblyPathList)}=null");
-            foreach (var fileAssemblyPath in fileAssemblyPathList)
-            {
-                if (assemblyDictionary.ContainsKey(fileAssemblyPath) == false)
-                {
-                    // FileAssembly
-                    var fileAssembly = System.Reflection.Assembly.LoadFrom(fileAssemblyPath);
-                    if (fileAssembly == null) throw new InvalidOperationException($"{nameof(fileAssembly)}=null");
-
-                    // Add
-                    assemblyDictionary[fileAssembly.Location] = fileAssembly;
-                }
-            }
-
-            // EntryAssembly
-            var entryAssembly = System.Reflection.Assembly.GetEntryAssembly();
-            if (entryAssembly != null)
-            {
-                if (assemblyDictionary.ContainsKey(entryAssembly.Location) == false)
-                {
-                    assemblyDictionary[entryAssembly.Location] = entryAssembly;
-                }
-            }
-
-            // AssemblyList 
-            var assemblyList = assemblyDictionary.Values.Where(assembly =>
-            {
-                // AssemblyName
-                var assemblyName = System.IO.Path.GetFileName(assembly.Location);
-                if (string.IsNullOrEmpty(assemblyName) == true) return false;
-
-                // Filter
-                if (assemblyName.StartsWith("System") == true) return false;
-                if (assemblyName.StartsWith("Microsoft") == true) return false;
-                if (assemblyName.StartsWith("_Microsoft") == true) return false;
-
-                // Filter(MAUI)
-                if (assemblyName.StartsWith("Xamarin") == true) return false;
-                if (assemblyName.StartsWith("Mono") == true) return false;
-
-                // Filter(MAUI.WinRT)
-                if (assemblyName.StartsWith("WinRT") == true) return false;
-
-                // Filter(MAUI.Android)
-                if (assemblyName.StartsWith("Java") == true) return false;
-                if (assemblyName.StartsWith("Google") == true) return false;
-                if (assemblyName.StartsWith("Windows") == true) return false;
-                if (assemblyName.StartsWith("mscorlib.dll") == true) return false;
-                if (assemblyName.StartsWith("netstandard.dll") == true) return false;
-                if (assemblyName.StartsWith("Jsr305Binding.dll") == true) return false;
-
-                // Return
-                return true;
-            }).ToList();
-
-            // Return
-            return assemblyList;
         }
     }
 }
